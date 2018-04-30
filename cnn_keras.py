f86423f288a9f253ce234d73c3b42a6fe1ff7bff
Manjunath M <manjunath.bsk3@gmail.com>
2018-04-30 13:02:14 +0530

    Removed 3rd layer

--- cnn_keras.py
+++ cnn_keras.py
@@ -28,9 +28,9 @@ def build_model(hight, weight, num_classes):
 
     model.add(Dropout(0.25))
     # # Layer 3
-    model.add(Conv2D(8, (5,5), padding="same"))
-    model.add(MaxPooling2D())
-    model.add(LeakyReLU(alpha=0.03))
+    # model.add(Conv2D(8, (5,5), padding="same"))
+    # model.add(MaxPooling2D())
+    # model.add(LeakyReLU(alpha=0.03))
 
     # model.add(Dropout(0.5))
     # Fully Connected Layer
