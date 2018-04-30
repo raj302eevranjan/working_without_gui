6ad4f1d209d9e94e2a01c96090352e6f50bb439e
Manjunath M <manjunath.bsk3@gmail.com>
2018-04-30 07:10:31 +0530

    Data Augmented and Model changed to accept grayscale image

--- cnn_keras.py
+++ cnn_keras.py
@@ -19,7 +19,7 @@ def build_model(hight, weight, num_classes):
     # Layer 1
     model.add(Conv2D(16, (3,3), activation='relu', padding="same", input_shape = (hight, weight, 1)))
     model.add(MaxPooling2D())
-    
+
     model.add(Dropout(0.2))
     # Layer 2
     model.add(Conv2D(32, (5,5), padding="same"))
@@ -34,22 +34,44 @@ def build_model(hight, weight, num_classes):
 
     model.add(Dropout(0.5))
     # Fully Connected Layer
-    
+
     model.add(Flatten())
     model.add(Dense(128))
     model.add(Activation('relu'))
 
     model.add(Dense(num_classes))
     model.add(Activation('softmax'))
-    
+
     return model
 
 
 def shuffle(x, y):
-    # TODO: Shuffle
-    pass
+    seed = np.random.randint(512, 1024, size=1)[0]
+    rand_state = np.random.RandomState(seed)
+    rand_state.shuffle(x)
+    rand_state.seed(seed)
+    rand_state.shuffle(y)
 
 def get_data(imageShape):
+
+    def read_from(filenames, label):
+        imgs = []
+        labels = []
+        for filename in filenames:
+            img = cv.imread('dataset/{}.pgm'.format(filename), cv.IMREAD_GRAYSCALE)
+            img = cv.resize(img, imageShape).reshape((imageShape[0], imageShape[1], 1))
+            img_1 = cv.imread('dataset/{}_1.pgm'.format(filename), cv.IMREAD_GRAYSCALE)
+            img_1 = cv.resize(img_1, imageShape).reshape((imageShape[0], imageShape[1], 1))
+            img_2 = cv.imread('dataset/{}_2.pgm'.format(filename), cv.IMREAD_GRAYSCALE)
+            img_2 = cv.resize(img_2, imageShape).reshape((imageShape[0], imageShape[1], 1))
+            img_3 = cv.imread('dataset/{}_3.pgm'.format(filename), cv.IMREAD_GRAYSCALE)
+            img_3 = cv.resize(img_3, imageShape).reshape((imageShape[0], imageShape[1], 1))
+            imgs.extend([img, img_1, img_2, img_3])
+            temp = [ label for _ in xrange(4) ]
+            labels.extend(temp)
+
+        return imgs, labels
+
     benign_file = open('dataset/benign.txt')
     malignant_file = open('dataset/malignant.txt')
     normal_file = open('dataset/normal.txt')
@@ -60,33 +82,32 @@ def get_data(imageShape):
 
     normal = [line.strip() for line in normal_file.readlines()]
 
-    benign_img = [ cv.imread('{}.pgm'.format(no)) for no in benign ]
-    benign_label = [ [0,1,0] for _ in xrange(len(benign)) ]
+    # benign_img = [ cv.imread('dataset/{}.pgm'.format(no)) for no in benign ]
+    # benign_label = [ [0,1,0] for _ in xrange(len(benign)) ]
+    benign_img, benign_label = read_from(benign, [0,1,0])
 
-    malignant_img = [ cv.imread('{}.pgm'.format(no)) for no in malignant ]
-    malignant_label = [ [0,0,1] for _ in xrange(len(malignant)) ]
+    # malignant_img = [ cv.imread('dataset/{}.pgm'.format(no)) for no in malignant ]
+    # malignant_label = [ [0,0,1] for _ in xrange(len(malignant)) ]
+    malignant_img, malignant_label = read_from(malignant, [0,0,1])
 
-    normal_img = [ cv.imread('{}.pgm'.format(no)) for no in normal ]
-    normal_label = [ [1,0,0] for _ in xrange(len(normal)) ]
+    # normal_img = [ cv.imread('dataset/{}.pgm'.format(no)) for no in normal ]
+    # normal_label = [ [1,0,0] for _ in xrange(len(normal)) ]
+    normal_img, normal_label = read_from(normal, [1,0,0])
 
     x = []
-    x.extend(benign_img)
     x.extend(malignant_img)
+    x.extend(benign_img)
     x.extend(normal_img)
-
-    # Reshaping image
-    # x = [ cv.resize(img, imageShape) for img in x ]
-    x =  [ resize(img, imageShape) for img in x ]
 
     y = []
     y.extend(benign_label)
     y.extend(malignant_label)
     y.extend(normal_label)
 
-    # TODO: Shuffle
+    shuffle(x, y)
 
-    x_train, x_test = x[:int(len(x)*0.7)], x[int(len(x)*0.3):] 
-    y_train, y_test = y[:int(len(x)*0.7)], y[int(len(x)*0.3):] 
+    x_train, x_test = x[:int(len(x)*0.7)], x[int(len(x)*0.3):]
+    y_train, y_test = y[:int(len(x)*0.7)], y[int(len(x)*0.3):]
 
     x_train = np.array(x_train, dtype = np.float32) / 255
     y_train = np.array(y_train, dtype = np.float32)
@@ -100,18 +121,20 @@ def get_data(imageShape):
 
 start = datetime.now()
 
+# Hyperparameters
+epochs = 50
+batch_size = 32
+imageShape = (70, 70)
+
 checkpointer = ModelCheckpoint(filepath='weights.hdf5', verbose=1, save_best_only=True)
 
-imageShape = (70, 70)
 model = build_model(imageShape[0], imageShape[1], 3)
 print('Done Building Model...')
 (x_train, y_train), (x_test, y_test) = get_data(imageShape)
 
-# Hyperparameters
-epochs = 50
-batch_size = 32
-
-model.compile(loss = keras.losses.categorical_crossentropy, optimizer = keras.optimizers.Adadelta(), metrics=['accuracy'])
+model.compile(  loss = keras.losses.categorical_crossentropy,
+                optimizer = keras.optimizers.Adadelta(),
+                metrics=['accuracy'])
 
 print('Done Compiling Model...')
 
